d3b981fbb4e5d8b2fa47744b0dd95efe9212908b
MarkVue1989 <markvazquezgarcia@gmail.com>
2024-06-29 16:32:49 +0200

    Opciones en la respuesta e enviar imagenes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 import os
 from dotenv import load_dotenv
 
@@ -19,9 +20,34 @@ def send_welcome(message):
 def send_help(message):
     bot.reply_to(message,'Puedes escribir cosas y yo las repetire.')
 
-@bot.message_handler(func=lambda m:True)
-def echo_all(message):
-    bot.reply_to(message,message.text)
+# @bot.message_handler(func=lambda m:True)
+# def echo_all(message):
+#     bot.reply_to(message,message.text)
 
+@bot.message_handler(commands=['opcion'])
+def send_options(message):
+    #Creación de grid para los botones.
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    #Creación de los botones.
+    btn_si = types.InlineKeyboardButton('Si', callback_data='opcion_si')
+    btn_no = types.InlineKeyboardButton('No', callback_data='opcion_no')
+    #Anadir botones a grid.
+    markup.add(btn_si,btn_no)
+    #bot.reply_to(message,'¿Te apetece?', reply_markup=markup)
+    bot.send_message(message.chat.id, "¿Te gusta la opción?", reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == 'opcion_si':
+        #bot.send_message(call.message.chat.id, "Perfecto!")
+        bot.answer_callback_query(call.id, text="Perfecto!")
+    elif call.data == 'opcion_no':
+        bot.answer_callback_query(call.id, text="No pasa nada!")
+        #bot.send_message(call.message.chat.id, "No pasa nada.")
+
+@bot.message_handler(commands=['foto'])
+def send_fotos(message):
+    img_url = 'https://pbs.twimg.com/profile_images/1806568875875561473/PF0vueCt_400x400.jpg'
+    bot.send_photo(chat_id=message.chat.id, photo=img_url, caption="Dame tu dinero facha")
 if __name__ == "__main__":
     bot.polling(none_stop=True)
